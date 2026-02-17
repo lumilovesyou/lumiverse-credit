1c0cb04f74e9fad28c28bb1675529716055a5010
mewo <177341604+lumilovesyou@users.noreply.github.com>
2026-02-17 14:27:42 -0600

    Fixed Search & Added Page

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function search() {
     const search = document.getElementById("search").value.toLowerCase();
-    const pagesDiv = document.getElementById("pages");
-    pages = pagesDiv.children;
-    for (let i = 0; i < pages.length; i++) {
-        console.log(pages[0].children[0].innerHTML);
-        if (pages[i].children[0].innerHTML.toLowerCase().includes(search)) {
-            pages[0].style.visibility = "visible";
+    Array.from(document.getElementsByTagName("p")).forEach((element) => {
+        console.log(element.children[0].innerHTML)
+        if (element.children[0].innerHTML.toLocaleLowerCase().includes(search) && search !== "") {
+            element.style.display = "none"
         } else {
-            pages[0].style.visibility = "hidden";
+            element.style.display = "inherit";
         }
-    }
+    });
 }
